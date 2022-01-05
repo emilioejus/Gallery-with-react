ac98931b7dfb8e9a3d3372928eacb48f33c2a126
emilioejus <emilioejus@gmail.com>
2022-01-05 12:34:17 +0100

    css was removed, navbar was modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ()=> {
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="collapse navbar-collapse  justify-content-end me-5" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                   <Link className="nav-link" to="/login">Login</Link>
                   <Link className="nav-link" to="/register">Register</Link>
